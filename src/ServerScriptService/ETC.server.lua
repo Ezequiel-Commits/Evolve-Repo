c12c2d79cbb762f870e7fe0c8d03c88030c51222
Ezequiel-Commits <142255412+Ezequiel-Commits@users.noreply.github.com>
2023-08-28 14:39:14 -0400

    Getting rid of a test for understanding

--- src/ServerScriptService/ETC.server.lua
+++ src/ServerScriptService/ETC.server.lua
@@ -19,17 +19,3 @@ for i, v in pairs(addressDatabase) do
 	print(i,v)
 end]]
 
--- trying to understand how a weld constraint works
-local partA = Instance.new("Part")
-local partB = Instance.new("Part")
-
-partA.Position = Vector3.new(0, 10, 0)
-partA.Parent = game.Workspace
-
-partB.Position = Vector3.new(0, 10, 10)
-partB.Parent = game.Workspace
-
-local weld = Instance.new("WeldConstraint")
-weld.Parent = partA
-weld.Part0 = partA
-weld.Part1 = partB
